f374c662547708b9d8e3918dafe94db80d9d17aa
Akash Bhave <akashbbhave@gmail.com>
2020-01-19 09:27:40 -0500

    Create alias map, setup redirects

--- main.go
+++ main.go
@@ -5,10 +5,23 @@ import (
 	"net/http"
 )
 
+// Map of [alias] -> [URL]
+var m = map[string]string{
+	"go":    "https://golang.org",
+	"jboss": "http://www.jboss.org/",
+	"akash": "https://akashbhave.com",
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "welcome to Akash's URL shortener")
 	})
+
+	for alias, url := range m {
+		http.HandleFunc("/"+alias, func(w http.ResponseWriter, r *http.Request) {
+			http.Redirect(w, r, url, 301)
+		})
+	}
 
 	http.ListenAndServe(":8800", nil)
 }
